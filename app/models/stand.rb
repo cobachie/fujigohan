16b8c04b96c26d7820241e75dac6998e3b34e145
cobachie <tanacobachie@gmail.com>
2018-07-23 12:46:36 +0900

    Update Stand Index

--- app/models/stand.rb
+++ app/models/stand.rb
@@ -1,3 +1,8 @@
 class Stand < ApplicationRecord
   validates :area, :name, presence: true
+
+  def image_url
+    frf_url = "http://www.fujirockfestival.com/news/"
+    "#{frf_url}#{image}"
+  end
 end
