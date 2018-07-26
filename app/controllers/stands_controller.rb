43a006af56a9833d24e3734cd4a0d85a8ab35c47
cobachie <tanacobachie@gmail.com>
2018-07-26 13:09:36 +0900

    no longer used

--- app/controllers/stands_controller.rb
+++ app/controllers/stands_controller.rb
@@ -33,60 +33,6 @@ class StandsController < ApplicationController
     redirect_to stands_url(tag: params[:tag])
   end
 
-  # GET /stands/1
-  # GET /stands/1.json
-  def show
-  end
-
-  # GET /stands/new
-  def new
-    @stand = Stand.new
-  end
-
-  # GET /stands/1/edit
-  def edit
-  end
-
-  # POST /stands
-  # POST /stands.json
-  def create
-    @stand = Stand.new(stand_params)
-
-    respond_to do |format|
-      if @stand.save
-        format.html { redirect_to @stand, notice: 'Stand was successfully created.' }
-        format.json { render :show, status: :created, location: @stand }
-      else
-        format.html { render :new }
-        format.json { render json: @stand.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /stands/1
-  # PATCH/PUT /stands/1.json
-  def update
-    respond_to do |format|
-      if @stand.update(stand_params)
-        format.html { redirect_to @stand, notice: 'Stand was successfully updated.' }
-        format.json { render :show, status: :ok, location: @stand }
-      else
-        format.html { render :edit }
-        format.json { render json: @stand.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /stands/1
-  # DELETE /stands/1.json
-  def destroy
-    @stand.destroy
-    respond_to do |format|
-      format.html { redirect_to stands_url, notice: 'Stand was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
-
   private
 
   def set_stand
@@ -104,10 +50,5 @@ class StandsController < ApplicationController
 
   def set_redis_val(ids)
     REDIS.set redis_key, ids.join(',')
-  end
-
-  # Never trust parameters from the scary internet, only allow the white list through.
-  def stand_params
-    params.require(:stand).permit(:area, :name, :description)
   end
 end
--- app/views/stands/_stand.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! stand, :id, :area, :name, :description, :created_at, :updated_at
-json.url stand_url(stand, format: :json)
